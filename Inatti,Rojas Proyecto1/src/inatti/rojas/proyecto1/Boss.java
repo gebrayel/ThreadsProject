556dd58fd47c37be6695368cbb0005b89148e785
gebrayel <gebrayel.inatti@correo.unimet.edu.ve>
2021-02-21 16:57:46 -0400

    kdfkja

--- Inatti,Rojas Proyecto1/src/inatti/rojas/proyecto1/Boss.java
+++ Inatti,Rojas Proyecto1/src/inatti/rojas/proyecto1/Boss.java
@@ -1,11 +1,34 @@
 package inatti.rojas.proyecto1;
-
+import java.util.concurrent.Semaphore;
 public class Boss extends Thread {
     
     private int dayDuration;
-    private int sleepTime=18;
-    private int workingTime=6;
+    private double sleepTime=24/18f;
+    private double workingTime=24/6f;
+    private Semaphore semDaysforDeploy;
+    public Boss(int dayDuration, Semaphore semDaysforDeploy){
+        
+        this.dayDuration = dayDuration;
+        this.semDaysforDeploy = semDaysforDeploy;
+        
+    }
     
-    
+    public void run(){
+        while(true){
+            
+            try{
+                System.out.println("el boss a mimir");
+                Thread.sleep(Math.round((dayDuration*1000)/sleepTime));
+                System.out.println("el boss a chambear");
+                semDaysforDeploy.acquire();
+                Main.DaysforDeploy--;
+                Thread.sleep(Math.round((dayDuration*1000)/workingTime));
+                semDaysforDeploy.release();
+                
+            }catch(Exception e){
+                System.out.println("pelo bola el boss");
+            }
+        }
+    }
     
 }
--- Inatti,Rojas Proyecto1/src/inatti/rojas/proyecto1/Manager.java
+++ Inatti,Rojas Proyecto1/src/inatti/rojas/proyecto1/Manager.java
@@ -1,5 +1,36 @@
 package inatti.rojas.proyecto1;
-
+import java.util.concurrent.Semaphore;
 public class Manager extends Thread {
+    
+    private int dayDuration;
+    private int loteDuration;
+    private double sleepTime=24/2f;
+    private Semaphore semDaysforDeploy;
+    public Manager(int dayDuration, int loteDuration, Semaphore semDaysforDeploy){
+        
+        this.dayDuration = dayDuration;
+        this.semDaysforDeploy = semDaysforDeploy;
+        this.loteDuration = loteDuration;
+    }
+    
+    public void run(){
+        while(true){
+            
+            try{
+                System.out.println("el boss a mimir");
+                Thread.sleep(Math.round((dayDuration*1000)/sleepTime));
+                System.out.println("el boss a chambear");
+                semDaysforDeploy.acquire();
+                if (Main.DaysforDeploy == 0){
+                    Main.DaysforDeploy+=loteDuration; 
+                    System.out.println("Se acabo lo que se daba");
+                }
+                semDaysforDeploy.release();
+                
+            }catch(Exception e){
+                System.out.println("pelo bola el boss");
+            }
+        }
+    }
     
 }
